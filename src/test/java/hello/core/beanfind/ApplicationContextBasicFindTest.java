18edc23b5698b6a837eb06d3c3709359d7bcbfe1
frozzun <eonjun0783@naver.com>
2025-01-18 20:08:12 +0900

    .

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -21,13 +21,13 @@ import static org.assertj.core.api.Assertions.*;
 class ApplicationContextBasicFindTest {
   AnnotationConfigApplicationContext ac = new
     AnnotationConfigApplicationContext(AppConfig.class);
-  @Test
-  @DisplayName("빈 이름으로 조회")
-  void findBeanByName() {
-    MemberService memberService = ac.getBean("memberService",
-      MemberService.class);
-    assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
-  }
+//  @Test
+//  @DisplayName("빈 이름으로 조회")
+//  void findBeanByName() {
+//    MemberService memberService = ac.getBean("memberService",
+//      MemberService.class);
+//    assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+//  }
   @Test
   @DisplayName("이름 없이 타입만으로 조회")
   void findBeanByType() {
