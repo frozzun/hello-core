c43a33970109c3ce306ed20c67def70b68771682
frozzun <eonjun0783@naver.com>
2025-02-21 18:18:36 +0900

    애노테이션 직접 만들기

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import static org.springframework.context.annotation.ComponentScan.*;
+
 @Configuration
 @ComponentScan(
   excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,22 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import io.micrometer.common.lang.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+  @Test
+  void AutowiredOption() {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+  }
+
+  static class TestBean{
+
+  }
+}
