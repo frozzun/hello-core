68cc5b24e542ae647598d0e80c5e1d4cf65853f1
frozzun <eonjun0783@naver.com>
2025-02-24 00:22:14 +0900

    스코프와 프록시

--- src/main/java/hello/core/config/DisCountPolicyConfig.java
+++ src/main/java/hello/core/config/DisCountPolicyConfig.java
@@ -1,24 +1,24 @@
-package hello.core.config;
-
-import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 수동 빈 등록
- */
-@Configuration
-public class DisCountPolicyConfig {
-
-  @Bean
-  public DiscountPolicy rateDiscountPolicy() {
-    return new RateDiscountPolicy();
-  }
-
-  @Bean
-  public DiscountPolicy fixDiscountPolicy() {
-    return new FixDiscountPolicy();
-  }
-}
+//package hello.core.config;
+//
+//import hello.core.discount.DiscountPolicy;
+//import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.RateDiscountPolicy;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 수동 빈 등록
+// */
+//@Configuration
+//public class DisCountPolicyConfig {
+//
+//  @Bean
+//  public DiscountPolicy rateDiscountPolicy() {
+//    return new RateDiscountPolicy();
+//  }
+//
+//  @Bean
+//  public DiscountPolicy fixDiscountPolicy() {
+//    return new FixDiscountPolicy();
+//  }
+//}
---
+++ src/main/java/hello/core/web/LogDemoInterseptor.java
@@ -0,0 +1,7 @@
+package hello.core.web;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LogDemoInterseptor implements HandlerInterceptor {
+
+}
